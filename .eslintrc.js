4b7750874f085a864783d5f6dfb85af69ea15f99
Adebayo Daramola <daramola.adebayo@yahoo.com>
2019-04-29 21:30:33 +0100

    chore(Setup): setup node environment and install necessary packages and dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,16 @@
 module.exports = {
     "extends": "airbnb-base",
+    "rules": {
+        "no-param-reassign": 0,
+        "no-unused-expressions": 0,
+        "radix": 0,
+        "no-console": 0
+    },
     "globals": {
         "window": true,
         "document": true
+    },
+    "env": {
+        "mocha": true
     }
 };
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+[![Build Status](https://travis-ci.org/Baystef/Quick-Credit.svg?branch=develop)](https://travis-ci.org/Baystef/Quick-Credit)
+
 # Quick-Credit
 Quick Credit is an online lending platform
