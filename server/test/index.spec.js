2bfb0a95b6466ae61830874cff5c3f5dd4fb1f52
Adebayo Daramola <daramola.adebayo@yahoo.com>
2019-04-30 10:04:46 +0100

    chore(babel): update babel packages and create test file

---
+++ server/test/index.spec.js
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+import server from '../index';
+
+describe('test', () => {
+  it('should return a string', () => {
+    expect('This is Quick Credit').to.equal('This is Quick Credit');
+  });
+});
